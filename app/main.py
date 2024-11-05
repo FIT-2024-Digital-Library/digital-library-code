d5825a8a5685c0a16ac133e6252ec99d24c767ca
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2024-11-05 13:03:23 +0700

    Remove last parts of SSR

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from app.templates_processor import templates
 from app.routes import books
 from app.routes import users
 
@@ -13,10 +11,3 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(books.router)
 app.include_router(users.router)
-
-
-@app.get("/", response_class=HTMLResponse)
-async def main_page(request: Request):
-    return templates.TemplateResponse(
-        request=request, name="pages/main-page.html", context={"books": ["a", "b", "c"]}
-    )
--- app/templates_processor.py
+++
@@ -1,5 +0,0 @@
-from fastapi.templating import Jinja2Templates
-
-templates = Jinja2Templates(directory="templates")
-
-# TODO: по хорошему, генераторы темплейтов вынести тоже сюда из хэндеров либо писать их отдельными функциями рядом с хэндлерами
