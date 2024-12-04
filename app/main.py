881a47e72a29648e39898786201c3465512f6850
Anton Tsoy <tcoy210@gmail.com>
2024-12-04 23:08:03 +0700

    Config minio

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.routes import books, users, authors, genres
+from app.routes import books, users, authors, genres, storage
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -9,6 +9,7 @@ app.include_router(books.router)
 app.include_router(users.router)
 app.include_router(authors.router)
 app.include_router(genres.router)
+app.include_router(storage.router)
 
 app.add_middleware(
     CORSMiddleware,
