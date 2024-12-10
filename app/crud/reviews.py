534dcd7134d5812430aaa50487f6204c72811da5
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2024-12-10 14:11:37 +0700

    Fix create crud operation

--- app/crud/reviews.py
+++ app/crud/reviews.py
@@ -4,6 +4,7 @@ from sqlalchemy import select, insert, delete, update
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.crud.books import get_book_from_db
 from app.models import review_table
 from app.schemas import Review, ReviewCreate
 from app.utils import CrudException
@@ -18,6 +19,9 @@ async def get_review_by_id(session: AsyncSession, review_id: int) -> Review:
 
 
 async def create_review_in_db(session: AsyncSession, user_id: int, review_data: ReviewCreate) -> Review:
+    book = await get_book_from_db(session, review_data.book_id)
+    if book is None:
+        raise ValueError("Book for review not found")
     result = await session.execute(
         insert(review_table)
         .values(author_id=user_id, last_edit_date=datetime.date.today(), **review_data.model_dump())
