039ecbc18a5c771fb1f19fc3e2f3abc06ba1dcb1
Anton Tsoy <tcoy210@gmail.com>
2024-12-17 23:08:27 +0700

    Start testing work of Elasticsearch

--- app/main.py
+++ app/main.py
@@ -32,21 +32,106 @@ app.include_router(reviews.router)
 ####################################################################
 ####################################################################
 
-
-from fastapi import UploadFile, File
+from elasticsearch import Elasticsearch
+from fastapi import BackgroundTasks, UploadFile, File
+#from sentence_transformers import SentenceTransformer
 import pdfplumber
 import io
 
+ELASTIC_API_PORT = 9200
 
-@app.post("/upload-pdf/")
-async def upload_file(file: UploadFile = File(...)):
-    # Читаем содержимое файла
-    contents = await file.read()
 
+"""
+# Загрузка модели
+__model = SentenceTransformer('all-MiniLM-L6-v2')  # Модель с размером вектора 384
+
+def encode_text_to_vector(text: str):
+    return __model.encode(text).tolist()  # Преобразуем в список для сохранения
+"""
+
+# Подключение к Elasticsearch
+es = Elasticsearch(f"http://localhost:{ELASTIC_API_PORT}")
+
+index_settings = {
+    "settings": {
+        "analysis": {
+            "analyzer": {
+                "default": {
+                    "type": "standard"
+                }
+            }
+        }
+    },
+    "mappings": {
+        "dynamic": "strict",
+        "properties": {
+            "title": {"type": "text"},
+            "author": {"type": "text"},
+            "genre": {"type": "keyword"},
+            "content": {"type": "text"}
+        }
+    }
+}
+
+def extract_pdf_text(content: bytes) -> str:
     # Используем io.BytesIO для создания файлового объекта из содержимого
-    with pdfplumber.open(io.BytesIO(contents)) as pdf:
+    with pdfplumber.open(io.BytesIO(content)) as pdf:
         full_text = ""
         for page in pdf.pages:
-            full_text += page.extract_text()
+            full_text += page.extract_text() + "\n"
+    print("BOOK-PROCESSING: Finish extracting")
+    return full_text
 
-    return {"filename": file.filename, "content": full_text}
+def index_book(book_id: str, title: str, author: str, genre: str, content: bytes):
+    if not es.indices.exists(index="books"):
+        es.indices.create(index="books", body=index_settings)
+    # Генерация вектора
+    # content_vector = encode_text_to_vector(content)
+    # Формирование документа
+    book_text: str = extract_pdf_text(content)
+    document = {
+        "title": title,
+        "author": author,
+        "genre": genre,
+        "content": book_text,
+    }
+    # Индексация с обработкой ошибок
+    try:
+        es.index(index="books", id=book_id, body=document)
+        print("BOOK-PROCESSING: Finish indexing")
+    except Exception as e:
+        print(f"Ошибка индексации: {e}")
+
+
+@app.post("/upload-pdf")
+async def upload_file(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
+    # Читаем содержимое файла
+    content = await file.read()
+    background_tasks.add_task(
+        index_book,
+        book_id="1",
+        title="Системные требования",
+        author="К.В. Козлов",
+        genre="страх и ненависть",
+        content=content
+    )
+    return {"filename": file.filename}
+
+
+def search_books(query):
+    search_body = {
+        "query": {
+            "multi_match": {
+                "query": query,
+                "fields": ["title^3", "author^2", "genre", "content"]
+            }
+        }
+    }
+    return es.search(index="books", body=search_body)
+
+
+@app.get("/find-book")
+def find_books(query: str):
+    results = search_books(query)
+    for hit in results['hits']['hits']:
+        print(hit["_source"]["title"])
